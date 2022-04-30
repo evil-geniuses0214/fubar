0048f36f29c1ffb33b5cb4d11cec39cd837fe9f7
CarRamos <goodsleep415@gmail.com>
2022-04-30 19:09:20 +0800

    rspec testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,8 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # https://stackoverflow.com/questions/15211576/rspec-authenticating-devise-user-in-request-specs
+  config.include Devise::Test::IntegrationHelpers, type: :request
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
