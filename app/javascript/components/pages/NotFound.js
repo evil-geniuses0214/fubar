72efbf5251cbdf369b69707a34af896959ccf3c0
Michael Scalise <99690810+scalisema@users.noreply.github.com>
2022-05-03 15:13:02 -0400

    Update NotFound.js

--- app/javascript/components/pages/NotFound.js
+++ app/javascript/components/pages/NotFound.js
@@ -7,7 +7,7 @@ export default class NotFound extends Component {
                 <h1>Page Not Found</h1>
                 <h1>This is Unacceptable!</h1>
                 <br></br>
-                <h2>Please email our fearless leader at: Ediie@evilgeniuses.com and he will dispatch severe punishment to the responsible developer </h2>
+                <h2>Please email our fearless leader at: Eddie@evilgeniuses.com and he will dispatch severe punishment to the responsible developer </h2>
             </>
         )
     }
